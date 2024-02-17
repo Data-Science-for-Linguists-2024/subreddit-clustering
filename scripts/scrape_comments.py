66dc352381fd47bf3bf7f62e0275c46f0082355c
maddy <32717754+ocilical@users.noreply.github.com>
2024-02-17 15:35:24 -0500

    write reddit scraper

---
+++ scripts/scrape_comments.py
@@ -0,0 +1,49 @@
+import json
+import os
+import praw
+from tqdm import tqdm  # progress bar since this will take a while
+
+with open("../private/reddit_private.json", "r") as f:
+    reddit_private = json.load(f)
+
+with open("../private/top_subreddits.txt", "r") as f:
+    sub_list = f.read().split()
+
+# use existing comment data if it exists
+# this is so i don't have to rerun the script in its entirety if i have to stop it unexpectedly
+comment_data = {}
+if os.path.exists("../private/comment_data.json"):
+    with open("../private/comment_data.json", "r") as f:
+        comment_data = json.load(f)
+
+reddit = praw.Reddit(
+    client_id=reddit_private["client_id"],
+    client_secret=reddit_private["client_secret"],
+    user_agent=reddit_private["user_agent"],
+    username=reddit_private["username"],
+    password=reddit_private["password"],
+)
+
+for sub_name in tqdm(sub_list[:10], position=0):
+    # skip over ones that were scrapped in a previous session
+    if sub_name in comment_data:
+        continue
+
+    # grab the top 10 posts of the past year
+    num_posts = 10
+    posts = reddit.subreddit(sub_name).top(limit=num_posts, time_filter="year")
+
+    comments = []
+    for post in tqdm(posts, position=1, leave=False, total=num_posts, desc=f"r/{sub_name}"):
+        # get rid of the weird non-comment objects
+        post.comments.replace_more(limit=0)
+        # this only gets top level comments
+        # also ignores automoderator since it tends to leave the same comment on every post and i don't want that in my data
+        comments.append("\n".join(
+            [comment.body for comment in post.comments if comment.author != "AutoModerator"]))
+
+    comment_data[sub_name] = "\n".join(comments)
+
+    # save per iteration just to be safe
+    with open("../private/comment_data.json", "w") as f:
+        json.dump(comment_data, f)
--- scripts/scrape_top_subreddits.py
+++ scripts/scrape_top_subreddits.py
@@ -1,18 +1,21 @@
 import argparse
 from bs4 import BeautifulSoup
 import requests
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument("num_pages", type=int, help="number of pages to scrape, 250 per page")
+parser.add_argument("num_pages", type=int,
+                    help="number of pages to scrape, 250 per page")
 parser.add_argument("-o", dest="output", help="output file")
 args = parser.parse_args()
 
 result = []
-for i in range(1, args.num_pages + 1):
+for i in tqdm(range(1, args.num_pages + 1)):
     page = requests.get(f"https://www.reddit.com/best/communities/{i}/")
     soup = BeautifulSoup(page.text, features="html.parser")
     sub_list = soup.find_all("div", {"class": "community-list"})
-    result.extend([link.get_text().strip() for link in sub_list[0].findChildren("a")])
+    result.extend([link.get_text().strip().split("/")[1]
+                  for link in sub_list[0].findChildren("a")])
 
 if args.output is None:
     print("\n".join(result))
