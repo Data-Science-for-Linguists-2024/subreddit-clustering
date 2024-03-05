f1d9a2459732ab09008fb372e96de4a5794091f9
maddy <32717754+ocilical@users.noreply.github.com>
2024-03-05 12:41:57 -0500

    add comments

--- scripts/unredact_data.py
+++ scripts/unredact_data.py
@@ -7,12 +7,13 @@ from tqdm import tqdm
 parser = argparse.ArgumentParser()
 parser.add_argument("in_file", help="file to unredact")
 parser.add_argument("out_file", help="file to write to")
-
 args = parser.parse_args()
 
+# load in redacted data
 with open(args.in_file, "r") as f:
     data = json.load(f)
 
+# load in reddit keys and set up instance
 with open("../private/reddit_private.json", "r") as f:
     reddit_private = json.load(f)
 
@@ -24,14 +25,18 @@ reddit = praw.Reddit(
     password=reddit_private["password"],
 )
 
+# get all ids from file and make list of fullnames
 ids = [f"t1_{entry["comment_id"]}" for entry in data]
 
+# use list of fullnaes to batch queries and populate dict that maps id to text
 id_dict = {}
 for c in tqdm(reddit.info(fullnames=ids), total=len(ids)):
     id_dict[c.id] = c.body
 
+# use id to text dict to fill in data
 for entry in data:
     entry["text"] = id_dict[entry["comment_id"]]
 
+# write unredacted data
 with open(args.out_file, "w") as f:
     json.dump(data, f, indent=4)
